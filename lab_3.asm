56778f723bb91907ca8838d0399fda85b0d9fc6d
IrinaTarasova <63746987+IrinaTarasova98@users.noreply.github.com>
2021-11-22 14:15:54 +0500

    Update lab_3.asm

--- lab_3.asm
+++ lab_3.asm
@@ -32,7 +32,7 @@ START:  ; метка начала программы
 		MOV 	R4, #0 			; позиция в строке
 		MOV 	R5, #5 			; число итераций
 DLOOP: ; выполнять очередной кадр пока не нажата клавиша
-		MOV		A, #0C7h 	; начать запись с 7 места второй строки
+		MOV	A, #0C7h 		; начать запись с 7 места второй строки
 		LCALL 	CDISP
 		MOV 	DPTR, #LEDS 		; получить список значений для линейки диодов
 		MOV 	A, R4 			; записать номер значения
@@ -45,10 +45,10 @@ DLOOP: ; выполнять очередной кадр пока не нажат
 BWAIT: ; выполнять проверку пока не нажата клавиша
 		LCALL	DELAY2			; время чтобы отпустить клавишу 
 		LCALL	KEY_DOWN		; проверка нажатия клавиши
-		JNZ 		BWAIT		; если клавиша не нажата - ждать (вернуться к метке BWAIT)
-		INC 		R4 		; прибавить единицу к индексу значения 
+		JNZ 	BWAIT			; если клавиша не нажата - ждать (вернуться к метке BWAIT)
+		INC 	R4 			; прибавить единицу к индексу значения 
 		DJNZ 	R5, DLOOP 		; если число итераций не равно нулю - начать новый кадр
-		SJMP		START 		; иначе начать программу заново
+		SJMP	START 			; иначе начать программу заново
 		RET
 ; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ;
         ;SJMP $ ; замкнутый бесконечный цикл
@@ -59,46 +59,46 @@ BWAIT: ; выполнять проверку пока не нажата клав
 ; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ;
 
 DISPA: ; вывод символа
-		MOV		DPP, #8
-		MOV		DPH, #0
-		MOV		DPL, #1
+		MOV	DPP, #8
+		MOV	DPH, #0
+		MOV	DPL, #1
 		; запись значения в регистр данных
 		MOVX	@DPTR, A
 		LCALL	DELAY1
 		; смена состояния регистра E
-		MOV		DPL, #6
-		MOV		A, #0Dh
+		MOV	DPL, #6
+		MOV	A, #0Dh
 		MOVX	@DPTR, A
 		LCALL	DELAY1
 		; запись в регистр управления 
-		MOV		A, #0Ch
+		MOV	A, #0Ch
 		MOVX 	@DPTR, A
 		LCALL	DELAY1
 		RET
 	
 CDISP: ; запись команд
-		MOV		DPP, #8
-		MOV		DPH, #0
-		MOV		DPL, #1
+		MOV	DPP, #8
+		MOV	DPH, #0
+		MOV	DPL, #1
 		; запись значения в регистр данных
 		MOVX	@DPTR, A
 		LCALL	DELAY2
 		; смена состояния регистра E
-		MOV		DPL, #6
-		MOV		A, #9
+		MOV	DPL, #6
+		MOV	A, #9
 		MOVX	@DPTR, A
 		LCALL	DELAY2
 		; запись в регистр управления 
-		MOV		A, #8
+		MOV	A, #8
 		MOVX 	@DPTR, A
 		LCALL	DELAY2
 		RET
 
 KEY_DOWN:  ; проверка нажатия клавиши * 
 		; клавиатура
-		MOV		DPL, #0
+		MOV	DPL, #0
 		MOV 	DPH, #0
-		MOV		DPP, #8
+		MOV	DPP, #8
 		; COL1 (11111110b)
 		MOV 	A, #0FEh
 		MOVX 	@DPTR, A
@@ -108,18 +108,18 @@ KEY_DOWN:  ; проверка нажатия клавиши *
 		RET
 
 SET_DIODE:  ; запись значения линейки светодиодов
-		MOV		DPL,#7
-		MOV		DPH,#0
-		MOV		DPP,#8
+		MOV	DPL,#7
+		MOV	DPH,#0
+		MOV	DPP,#8
 		; запись значения из A в регистр светодиодов
 		MOVX	@DPTR, A 
 		LCALL	DELAY1
 		RET
 
 DELAY1:  ; подпрограмма задержки 70 мс
-   		MOV		R1, #10
+   		MOV	R1, #10
 D2131:
-		MOV		R0, #10
+		MOV	R0, #10
 		DJNZ	R0, $
 		DJNZ	R1, D2131
 		RET
